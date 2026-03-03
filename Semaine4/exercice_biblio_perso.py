2dbbaa9c497618486028aa7fe3ba90b22bc51019
Rimao416 <kayumbaomari27@gmail.com>
2026-03-03 22:18:12 +0100

    :rocket: Finalisation de l'exercice

--- Semaine4/exercice_biblio_perso.py
+++ Semaine4/exercice_biblio_perso.py
@@ -31,6 +31,69 @@ def afficher_livres():
         return # Le programme s'arrete ici
     for i, livre in enumerate(bibliotheque, start=1):
         statut= "Lu" if livre["lu"] else "Non lu"
+        print(f"{i}. {livre['titre']} - {livre['auteur']} ({livre['annee']}) [{statut}]")
+        
+def marquer_comme_lu(titre):
+    for livre in bibliotheque:
+        if livre['titre'].lower()== titre.lower():
+            livre['lu']=True
+            print(f"Livre {titre} marqué comme Lu")
+    print(f"Livre non trouvé")
+
+
+def compter_livres():
+    lus=sum(1 for livre in bibliotheque if livre["lu"])
+    non_lus=len(bibliotheque) - lus
+    
+    print(f"Lus : {lus}")
+    print(f"Non Lus : {non_lus}")
+    
+
+def chercher_par_auteur(auteur):
+    resultats=[livre for livre in bibliotheque if livre['auteur'].lower()== auteur.lower()]
+    if not resultats:
+        print(f"Aucun livre trouvé pour l'auteur {auteur}")
+        return 
+    print(f"Livre(s) de {auteur}")
+    for livre in resultats:
+        statut="Lu" if livre["lu"] else "Non lu"
+        print(f" - {livre['titre']} ({livre['annee']}) [{statut}]")
+
+def afficher_par_annee(annee):
+    resultats=[livre for livre in bibliotheque if livre['annee'] == annee]
+    if not resultats:
+        print(f"Aucun livre trouvé pour l'année {annee}")
+        return
+    print(f"Livre(s) de {annee}")
+    for livre in resultats:
+        statut="Lu" if livre["lu"] else "Non lu"
+        print(f" - {livre['titre']} ({livre['annee']}) [{statut}]")
+    
+
+if __name__=="__main__":
+    ajouter_livre("1984","George Orwell",1949)
+    ajouter_livre("L'Alchimiste","Paulo Coelho",1988,True)
+    ajouter_livre("Clean Code","Robert C.Martin",2008)
+    
+    print("\n --- Tous les livres ---")
+    afficher_livres()
+    
+    print("\n --- Marquer comme lu ---")
+    marquer_comme_lu("1984")
+    
+    print("\n --- Recherche par auteur ---")
+    chercher_par_auteur("Paulo Coelho")
+    
+    print("\n --- Compter les livres ---")
+    compter_livres()
+    
+    print("\n --- Livres par année ---")
+    afficher_par_annee(2008)
+    
+        
+    
+        
+        
         
     
     
